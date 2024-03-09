a9d94a82df208cae95dabd9fec98ccbefd0bcd64
Papillon6814 <so1rou.k@gmail.com>
2024-03-09 15:55:22 +0900

    feat: button appearance

--- src/components/calendar.rs
+++ src/components/calendar.rs
@@ -1,5 +1,6 @@
 use yew::*;
 use crate::components::menu::Menu;
+use crate::constants::colors::BG_BASE;
 use chrono::*;
 
 #[derive(Properties, Clone, PartialEq)]
@@ -25,12 +26,15 @@ fn month_view(props: &MonthViewProps) -> Html {
 	let total_slots = days_in_month + start_padding;
 	let weeks = (total_slots as f32 / 7.0).ceil() as i32;
 
+	let weekday_style = "font-family: DIN;";
+	let date_style = "width: 40px; height: 40px; text-align: center; font0family: DIN;";
+
 	html! {
 		<>
 			<table>
 				<thead>
 					<tr>
-						{weekdays.iter().map(|weekday| html! {<th>{weekday}</th>}).collect::<Html>()}
+						{weekdays.iter().map(|weekday| html! {<th style={weekday_style}>{weekday}</th>}).collect::<Html>()}
 					</tr>
 				</thead>
 				<tbody>
@@ -42,9 +46,9 @@ fn month_view(props: &MonthViewProps) -> Html {
 								for (0..7).map(|weekday| {
 									let day = week * 7 + weekday + 1 - start_padding;
 									if day > 0 && day <= days_in_month {
-										html! {<td>{day}</td>}
+										html! {<td style={date_style}>{day}</td>}
 									} else {
-										html! {<td></td>}
+										html! {<td style={date_style}></td>}
 									}
 								})
 							}
@@ -81,14 +85,21 @@ pub fn calendar() -> Html {
 		}
 	});
 
+	let main_style = "padding-top: 20px;";
+	let button_style = format!("background-color: {}; outline: none; border: 1px; height: 36px; width: 36px; border-radius: 6px; cursor: pointer;", BG_BASE);
+
 	html! {
 		<div style={"display: flex; margin: 0;"}>
 			<Menu />
-			<main>
-				<div style={"display: flex;"}>
-					<button onclick={on_prev}>{"<"}</button>
-					<h1>{format!("{} {}", *year, *month)}</h1>
-					<button onclick={on_next}>{">"}</button>
+			<main style={main_style}>
+				<div style={"display: flex; justify-content: space-between; align-items: center;"}>
+					<button style={button_style.clone()} onclick={on_prev}>
+						<img src="/public/icons/chevron-left.svg" />
+					</button>
+					<h1 style={"font-family: DIN;"}>{format!("{} {}", *year, *month)}</h1>
+					<button style={button_style.clone()} onclick={on_next}>
+						<img src="/public/icons/chevron-right.svg" />
+					</button>
 				</div>
 				<MonthView year={*year} month={*month} />
 			</main>
