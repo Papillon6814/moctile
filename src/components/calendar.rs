6fa6a67c1b8e49a955d4e7e866926b5574a91270
Papillon6814 <so1rou.k@gmail.com>
2024-03-09 11:38:18 +0900

    feat: add dates

--- src/components/calendar.rs
+++ src/components/calendar.rs
@@ -12,7 +12,23 @@ struct MonthViewProps {
 fn month_view(props: &MonthViewProps) -> Html {
 	let MonthViewProps { year, month } = props;
 
+	let first_day_of_month = NaiveDate::from_ymd_opt(*year, *month, 1).unwrap();
+	let first_weekday = first_day_of_month.weekday();
+	let days_in_month = first_day_of_month.with_month(*month + 1).unwrap().pred_opt().unwrap().day();
+
+	let mut days: Vec<Html> = Vec::new();
+	let mut day_count = 1;
+
 	let weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+	for _ in 0..first_weekday.num_days_from_sunday() {
+		days.push(html! {<td></td>});
+	}
+
+	while day_count <= days_in_month {
+		days.push(html! {<td>{day_count}</td>});
+		day_count += 1;
+	}
 
 	html! {
 		<>
@@ -20,9 +36,14 @@ fn month_view(props: &MonthViewProps) -> Html {
 			<table>
 				<thead>
 					<tr>
-					{weekdays.iter().map(|weekday| html! {<th>{weekday}</th>}).collect::<Html>()}
+						{weekdays.iter().map(|weekday| html! {<th>{weekday}</th>}).collect::<Html>()}
 					</tr>
 				</thead>
+				<tbody>
+					<tr>
+						{for days.into_iter()}
+					</tr>
+				</tbody>
 			</table>
 		</>
 	}
